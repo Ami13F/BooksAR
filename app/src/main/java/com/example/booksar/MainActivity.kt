ac902521bca482092a77af16fd1d0b74a62585ea
Amelia Corina Forgacs <ameliaforgacs@gmail.com>
2022-03-03 20:40:04 +0200

    [impl] Open camera and detect surface
    - add manifest and ARFragment

--- app/src/main/java/com/example/booksar/MainActivity.kt
+++ app/src/main/java/com/example/booksar/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.booksar.helpers.CameraPermissionHelper
-import com.google.ar.core.Session
+import com.example.booksar.models.Book
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/booksar/models/Book.kt
@@ -0,0 +1,17 @@
+package com.example.booksar.models
+
+import android.graphics.Color
+import com.google.ar.sceneform.math.Quaternion
+import com.google.ar.sceneform.math.Vector3
+
+data class Book (
+    val pages: Int?,
+    val cover: String,
+    val size: Vector3,
+    val position: Vector3,
+    val rotation: Quaternion,
+    val coverUrl: String,
+    var coverWidth: Int = 1,
+    var coverHeight: Int = 1,
+    var coverColor: Int = Color.WHITE
+    )
