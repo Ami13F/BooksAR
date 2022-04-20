b262b79afd3333f8a5f8cdcc6af42fdfdbea50c2
Amelia Corina Forgacs <ameliaforgacs@gmail.com>
2022-04-20 22:18:53 +0300

    [impl] Add share library option

--- app/src/main/java/com/example/booksar/MainActivity.kt
+++ app/src/main/java/com/example/booksar/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.booksar
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.provider.MediaStore
+import android.view.PixelCopy
 import androidx.appcompat.app.AppCompatActivity
 import com.example.booksar.core.BookArService
 import com.example.booksar.core.BooksArFragment
 import com.example.booksar.core.FirebaseService
 import com.example.booksar.models.Book
 import com.github.clans.fab.FloatingActionButton
+import com.google.ar.sceneform.ArSceneView
 import com.google.firebase.FirebaseApp
 
 
@@ -53,10 +59,34 @@ class MainActivity : AppCompatActivity() {
 
         val shareButton = findViewById<FloatingActionButton>(R.id.shareBtn)
         shareButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.type = "text/plain"
-            val chooser = Intent.createChooser(intent, "Share library...")
-            startActivity(chooser)
+            takeScreenShot(mainFragment.arSceneView)
         }
+    }
+
+    private fun takeScreenShot(view: ArSceneView): Bitmap? {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        PixelCopy.request(
+            view,
+            bitmap, { copyResult: Int ->
+                if (copyResult == PixelCopy.SUCCESS) {
+                    shareContent(bitmap)
+                }
+            }, Handler()
+        )
+        return bitmap
+    }
+
+    private fun shareContent(bitmap: Bitmap) {
+        val bitmapPath: String = MediaStore.Images.Media.insertImage(
+            contentResolver, bitmap, "Share", "Share this with: "
+        )
+
+        val uri = Uri.parse(bitmapPath)
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "image/*"
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "App")
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Look at my virtual library.")
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+        startActivity(Intent.createChooser(shareIntent, "Share"))
     }
 }
